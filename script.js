02121232c4406886f9e4ba80e5608e6c2af08e79
Tung No <111445323+tungno@users.noreply.github.com>
2023-05-28 17:03:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+function toggleContent() {
+    var content = document.getElementById("tableOfContents");
+    if (content.style.display === "none") {
+      content.style.display = "block";
+    } else {
+      content.style.display = "none";
+    }
+  }
+
+  function toggleContent1() {
+    var content = document.getElementById("tableOfContents1");
+    if (content.style.display === "none") {
+      content.style.display = "block";
+    } else {
+      content.style.display = "none";
+    }
+  }
+
+  function toggleContent2() {
+    var content = document.getElementById("tableOfContents2");
+    if (content.style.display === "none") {
+      content.style.display = "block";
+    } else {
+      content.style.display = "none";
+    }
+  }
+
+  function toggleContent3() {
+    var content = document.getElementById("tableOfContents3");
+    if (content.style.display === "none") {
+      content.style.display = "block";
+    } else {
+      content.style.display = "none";
+    }
+  }
+
+  function toggleContent4() {
+    var content = document.getElementById("tableOfContents4");
+    if (content.style.display === "none") {
+      content.style.display = "block";
+    } else {
+      content.style.display = "none";
+    }
+  }
